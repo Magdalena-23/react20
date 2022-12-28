c3b61785cd2d7aea8a4a06929d1a7ca3b3954f12
Magdalena-23 <114959243+Magdalena-23@users.noreply.github.com>
2022-12-28 21:00:17 +0100

    cas5

--- src/components/LifecycleFuncComponent.jsx
+++ src/components/LifecycleFuncComponent.jsx
@@ -1,13 +1,24 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const LifecycleFuncComponent = () => {
 
     const [counter, setCounter] = useState(5)
     // useState(inicijalna vrednost) - zachuvuvanje dinamichka vrednost na komponenta pomegju renderi [vrednost, funkcija(preku koja se menuva vrednosta)]
 
+    // rerenderiranje samo vo 2 sluchai : pri promena na state i props
+
     const increment = () => {
         setCounter(counter + 10)
     }
+
+    useEffect(() => {
+
+        console.log("like a componentDidMount")
+
+        return () => {
+            console.log("like a componentWillUnmount")
+        }
+    }, [])
 
     return <div>
         counter : {counter}
